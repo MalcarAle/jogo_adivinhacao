91dc3e26910041a342ffbaa25dd0ec6423f115b4
MalcarAle <alexandre_malcar@icloud.com>
2023-05-23 15:58:44 -0300

    aprimoramento da função handleTryClick

--- script.js
+++ script.js
@@ -3,35 +3,45 @@ const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-const randomNumber = Math.round(Math.random() * 10)
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
 //eventos
 btnTry.addEventListener("click", handleTryClick)
 btnReset.addEventListener("click", handleResetClick)
-
+document.addEventListener("keydown", enterReset)
 //função callback
 function handleTryClick(event) {
   event.preventDefault() //qnao faça o padrao
 
   const inputNumber = document.querySelector("#inputNumber")
-
-  if (Number(inputNumber.value) == randomNumber) {
-    toggleScreen()
-    document.querySelector(
-      ".screen2 h2"
-    ).innerText = `Acertou em ${xAttempts} tentativas`
+  if (Number(inputNumber.value) > 0) {
+    if (Number(inputNumber.value) == randomNumber) {
+      toggleScreen()
+      document.querySelector(
+        ".screen2 h2"
+      ).innerText = `Acertou em ${xAttempts} tentativas`
+    }
+    inputNumber.value = ""
+    xAttempts++
+  } else if (Number(inputNumber.value) < 0) {
+    alert("Você precisa coloca um número maior que 0")
   }
-  inputNumber.value = ""
-  xAttempts++
 }
 
 function handleResetClick() {
   toggleScreen()
   xAttempts = 1
+  randomNumber = Math.round(Math.random() * 10)
 }
 
 function toggleScreen() {
   screen1.classList.toggle("hide")
   screen2.classList.toggle("hide")
+}
+
+function enterReset(e) {
+  if (e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick()
+  }
 }
